5a90518a8c73072462a394aa6c0b54717c206034
Hebo <doubletime@gmail.com>
2013-10-19 14:39:35 -0700

    timeout between connections

--- src/irc_loadtest/core.clj
+++ src/irc_loadtest/core.clj
@@ -1,13 +1,13 @@
 (ns irc-loadtest.core
   (:gen-class)
   (:require [aleph.tcp :as tcp]
-            [lamina.core :as lamina.core]
+            [lamina.core :as lamina]
             [gloss.core :as gloss]
             [clojure.tools.cli :refer [cli]]))
 
 (defn connect
   [opts]
-  (lamina.core/wait-for-result
+  (lamina/wait-for-result
     (tcp/tcp-client {:host (:host opts),
                      :port (:port opts),
                      :frame (gloss/string :utf-8 :delimiters ["\n" "\r\n"])})))
@@ -15,29 +15,22 @@
 (defn handle-line
     "Handle lines
     TODO: respond to pings"
-    [ch line]
-    (println line))
+    [line]
+    ; (println line))
+    )
+
+(defn handle-close
+  []
+  (println "channel closed!"))
 
 (defn connect-and-send
-  [conn-opts msgs]
+  [id conn-opts msgs]
+  (println "Client" id "connecting!")
   (let [ch (connect conn-opts)]
-    (lamina.core/receive-all ch (partial handle-line ch))
-    (apply lamina.core/enqueue ch msgs)
+    (lamina/receive-all ch handle-line)
+    (lamina/on-closed ch handle-close)
+    (apply lamina/enqueue ch msgs)
     ch))
-
-
-(defn print-status
-  [channels]
-  (println (str "Successful connections: " (count (filter (complement lamina.core/closed?)
-                                                    channels))))
-
-  ; MEMORY LEEK!
-  ; Wait on channels to exit
-  ; (->> (apply lamina.core/merge-channels channels)
-  ;      (partial lamina.core/wait-for-message)
-  ;      (while true)))
-
-  )
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -46,6 +39,7 @@
                 ["-p" "--port" "Listen on this port" :parse-fn #(Integer. %)]
                 ["-h" "--host" "The hostname"]
                 ["-c" "--clients" "Number of clients" :default 1 :parse-fn #(Integer. %)])]
-    (-> (repeatedly (:clients opts)
-                (partial connect-and-send opts msgs))
-        (print-status))))
+    (dotimes [n (:clients opts)]
+                    (Thread/sleep 1000)
+                    (connect-and-send n opts msgs))
+    (println "Connections done!")))
